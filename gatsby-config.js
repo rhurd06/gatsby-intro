b5ff16d2b41b6e72837cdaf2d51ca449e6040d3e
Robyn Noble <robynhurd09@gmail.com>
2021-07-27 12:21:28 -0500

    added a contact page

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
         title: 'Frontend Masters Gatsby Workshop',
         description: 'A site we built together during a full-day Frontend Gatsby Workshop!'
     },
-    plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+    plugins: ['gatsby-plugin-emotion',
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-plugin-mdx',
+            options: {
+                defaultLayouts: {
+                    default: require.resolve('./src/components/layout.js'),
+                },
+            },
+        },
+    ],
 };

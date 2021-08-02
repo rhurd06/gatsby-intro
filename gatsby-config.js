546f46d86bc37ca05c3815ec36fd67e6fffd2cc5
Robyn Noble <robynhurd09@gmail.com>
2021-08-02 11:47:13 -0500

    made changes to files/paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
             resolve: 'gatsby-source-filesystem',
             options: {
                 name: 'images',
-                path: 'static',
+                path: 'images',
             },
         },
     ],
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Link, graphql, useStaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
 
-const ImageBackground = styled('div')`
-    background-image: url('/images/nicole-mn.jpg');
+const ImageBackground = styled('BackgroundImage')`
     background-position: top 20% center;
     background-size: cover;
     height: 50vh;
@@ -39,8 +39,19 @@ const TextBox = styled('div')`
 `;
 
 const Hero = () => {
+    const { image } = useStaticQuery(graphql`
+        query {
+            image: file(relativePath: {eq: "nicole-mn.jpg" }) {
+                sharp: childImageSharp {
+                    fluid {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+        }
+        `)
     return (
-        <ImageBackground>
+        <ImageBackground Tag="section" fluid={image.sharp.fluid} fadeIn="soft">
         <TextBox>
             <h1>Frontend Masters + Gatsby &hearts;</h1>
             <p>
